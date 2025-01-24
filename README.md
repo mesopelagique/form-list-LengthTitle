0103823592a55bda441c5314d0fa70c2cc7ffe34
eric.marchand <eric.marchand@4d.com>
2025-01-24 14:12:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Length title
+
+Clone of SimpleTable template that a dynamic title to display list length.
+
+:warning: Android only
+
+## how it work
+
+use a table label with `%length%`
+
+`%length%` will be replace by entity list length.
---
+++ android/app/src/main/java/__PKG__/list/EntityListFragment__TEMPLATE__.kt
@@ -0,0 +1,39 @@
+/*
+ * Created by {{author}} on {{date_day}}/{{date_month}}/{{date_year}}.
+ * {{company_header}}
+ * Copyright (c) {{date_year}} {{author}}. All rights reserved.
+ */
+
+package {{package}}.list
+
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.ViewDataBinding 
+import androidx.fragment.app.findFragment
+import com.qmobile.qmobiledatasync.utils.CustomEntityListFragment
+import com.qmobile.qmobileui.databinding.FragmentListBinding
+import com.qmobile.qmobileui.list.EntityListFragment
+import com.qmobile.qmobileui.ui.setupToolbarTitle
+
+class EntityListFragment{{tableName}}(private val binding: ViewDataBinding) : CustomEntityListFragment {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val fragment = view.findFragment<EntityListFragment>() // the caller
+        var title = fragment.arguments?.getString("navbarTitle")?: ""
+
+        if (!title.contains("%length%")) {
+            return
+        }
+        val listData = (binding as? FragmentListBinding)?.viewModel?.dao?.getAll()
+        listData?.observe(fragment.viewLifecycleOwner) {
+            val count = it.count()
+            fragment.activity?.setupToolbarTitle(title.replace("%length%", " $count"))
+        }
+        val count = listData?.value?.count() ?: 0
+        fragment.activity?.setupToolbarTitle(title.replace("%length%", " $count"))
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        onViewCreated(binding.root, savedInstanceState)
+    }
+}
